f1245a9d96ae9d8eded95abe6e280a8502a135ff
abinader89 <abinader.g@husky.neu.edu>
2019-03-05 15:38:42 -0500

    abinader89: add new fields and relationship.

--- priv/repo/migrations/20190228032951_create_users.exs
+++ priv/repo/migrations/20190228032951_create_users.exs
@@ -5,10 +5,13 @@ defmodule TaskTracker.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :name, :string
       add :email, :string
+      add :admin, :boolean
+      add :supervisor_id, references(:users, on_delete: :nothing)
 
       timestamps()
     end
 
     create unique_index(:users, [:email])
+    create index(:users, [:supervisor_id])
   end
 end
